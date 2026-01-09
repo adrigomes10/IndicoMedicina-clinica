19be95f3ed4a34f8efa5c69dabd5c5c5dd0cc3f6
adrigomes10 <adrigomes_1997@hotmail.com>
2026-01-09 09:16:39 -0300

    correcao de formulario

--- script.js
+++ script.js
@@ -5,14 +5,10 @@ const formulario = document.getElementById("form-agendamento");
 if (formulario) {
   formulario.addEventListener("submit", function (event) {
     const nome = document.querySelector("input[placeholder='Nome completo']").value.trim();
-    const email = document.querySelector("input[placeholder='E-mail']").value.trim();
     const telefone = document.querySelector("input[placeholder='Telefone']").value.trim();
 
     if (nome.length < 3) {
       alert("Por favor, insira um nome válido.");
-      event.preventDefault();
-    } else if (!email.includes("@") || email.length < 5) {
-      alert("Por favor, insira um e-mail válido.");
       event.preventDefault();
     } else if (telefone.length < 8) {
       alert("Por favor, insira um telefone válido.");
